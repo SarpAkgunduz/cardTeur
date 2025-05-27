c6eaf136b373aed9ae39191081baa624e19b69f3
Sarp Akgündüz <sakgunduz@proven.com.tr>
2025-05-27 16:39:18 +0300

    main page visual edits

--- openteur/src/App.tsx
+++ openteur/src/App.tsx
@@ -4,29 +4,23 @@ import PlayersPage from './pages/PlayersPage';
 import PreviewPage from './pages/PreviewPage';
 import MatchPage from './pages/MatchPage';
 import AddPlayerForm from './pages/AddPlayerForm';
-
+import './App.css';
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="container mt-5">
-      <h1 className="text-center mb-5">Welcome to Cardteur</h1>
-      <div className="row justify-content-center">
-        <div className="col-md-3 d-grid mx-2 mb-3">
-          <button className="btn btn-primary btn-lg" onClick={() => navigate('/edit')}>
-            Edit Players
-          </button>
+    <div className="container col-9 mt-5 body">
+      <div className="welcome mb-5"></div>
+      <div className="row ">
+        <div className="col-sm d-grid mb-3">
+          <button className="btn btn-lg btn-homepage btn-player-edit btn-home" onClick={() => navigate('/edit')}></button>
         </div>
-        <div className="col-md-3 d-grid mx-2 mb-3">
-          <button className="btn btn-success btn-lg" onClick={() => navigate('/preview')}>
-            Preview Cards
-          </button>
+        <div className="col-sm d-grid mb-3">
+          <button className="btn btn-lg btn-homepage btn-player-preview btn-home" onClick={() => navigate('/preview')}></button>
         </div>
-        <div className="col-md-3 d-grid mx-2 mb-3">
-          <button className="btn btn-warning btn-lg" onClick={() => navigate('/match')}>
-            Prepare Match
-          </button>
+        <div className="col-sm d-grid mb-3">
+          <button className="btn btn-lg btn-homepage btn-prepare-match btn-home" onClick={() => navigate('/match')}></button>
         </div>
       </div>
     </div>
