ceaf934923466a8aeeba28cfebfe374e66f9efc6
Sarp Akgündüz <sakgunduz@proven.com.tr>
2025-05-28 17:57:03 +0300

    for future page division bootstrap update

--- openteur/src/App.tsx
+++ openteur/src/App.tsx
@@ -10,19 +10,23 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="container col-9 mt-5 body">
-      <div className="welcome mb-5"></div>
-      <div className="row ">
-        <div className="col-sm d-grid mb-3">
-          <button className="btn btn-lg btn-homepage btn-player-edit btn-home" onClick={() => navigate('/edit')}></button>
-        </div>
-        <div className="col-sm d-grid mb-3">
-          <button className="btn btn-lg btn-homepage btn-player-preview btn-home" onClick={() => navigate('/preview')}></button>
-        </div>
-        <div className="col-sm d-grid mb-3">
-          <button className="btn btn-lg btn-homepage btn-prepare-match btn-home" onClick={() => navigate('/match')}></button>
+    <div className="row">
+
+      <div className="container col-9 mt-5 body">
+        <div className="welcome mb-5"></div>
+        <div className="row ">
+          <div className="col-sm d-grid mb-3">
+            <button className="btn btn-lg btn-homepage btn-player-edit btn-home" onClick={() => navigate('/edit')}></button>
+          </div>
+          <div className="col-sm d-grid mb-3">
+            <button className="btn btn-lg btn-homepage btn-player-preview btn-home" onClick={() => navigate('/preview')}></button>
+          </div>
+          <div className="col-sm d-grid mb-3">
+            <button className="btn btn-lg btn-homepage btn-prepare-match btn-home" onClick={() => navigate('/match')}></button>
+          </div>
         </div>
       </div>
+      
     </div>
   );
 };
