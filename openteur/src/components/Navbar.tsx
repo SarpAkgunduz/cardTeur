184412c80d75265c4135acc7c6d976c772e1af11
Sarp Akgündüz <sarpakg@gmail.com>
2026-09-10 23:13:29 +0300

    Mobile integration

--- openteur/src/components/Navbar.tsx
+++ openteur/src/components/Navbar.tsx
@@ -71,17 +71,19 @@ const Navbar = () => {
           <div className="ct-nav__logo" onClick={() => navigate('/')}>
             CardTeur
           </div>
-          <div className="ct-nav__links">
-            {(loggedIn ? NAV_LINKS : NAV_LINKS.filter(l => l.path === '/pricing')).map(({ path, labelKey }) => (
-              <button
-                key={path}
-                className={`ct-nav__link ${isActive(path) ? 'ct-nav__link--active' : ''}`}
-                onClick={() => navigate(path)}
-              >
-                {t(labelKey)}
-              </button>
-            ))}
-          </div>
+          {loggedIn && (
+            <div className="ct-nav__links">
+              {NAV_LINKS.map(({ path, labelKey }) => (
+                <button
+                  key={path}
+                  className={`ct-nav__link ${isActive(path) ? 'ct-nav__link--active' : ''}`}
+                  onClick={() => navigate(path)}
+                >
+                  {t(labelKey)}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
 
         <div className="ct-nav__right">
