834da228061033aa9dba26844ce9a95d0f0b5ef9
Sarp Akgündüz <sarpakg@gmail.com>
2026-09-18 22:41:20 +0300

    capital I update

--- openteur/src/components/Navbar.tsx
+++ openteur/src/components/Navbar.tsx
@@ -105,7 +105,7 @@ const Navbar = () => {
                     onClick={() => { i18n.changeLanguage(lang.code); setLangOpen(false); }}
                   >
                     <span className="ct-nav__lang-code">{lang.code.toUpperCase()}</span>
-                    <span lang={lang.code}>{lang.label}</span>
+                    <span lang={lang.code}>{lang.label.toLocaleUpperCase(lang.code)}</span>
                   </button>
                 ))}
               </div>
